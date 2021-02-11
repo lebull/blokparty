c151f8efa7ecdb677873ac38657390a52fa23e5b
lebull <bulldogboy912000@yahoo.com>
2021-02-11 16:34:03 -0600

    Multi-feature drifting

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import { on } from 'events';
-import { AudioStream } from './util/audioStream';
-import { IBand } from './util/audioStream';
-import SettableBand from './components/band/Band';
 import ButtplugComp from "./components/buttplug/ButtplugComp";
-import MusicAnalyzer from './components/music/MusicAnalyzer';
-
-
-const audioStream = new AudioStream();
-const INTERVAL_FPS = 12;
 
 function App() {
-
-  const [audioDevices, setAudioDevices] = useState<MediaDeviceInfo[]>([]);
-  const [selectedDevice, setSelectedDevice] = useState<MediaDeviceInfo>();
-  const [bands, setBands] = useState<IBand[]>([]);
-  const [tickNum, setTickNum] = useState<Number>(0);
-
-  const refreshAudioDevices = async () => {
-    await navigator.mediaDevices.getUserMedia({audio: true});
-    const devices = await (await navigator.mediaDevices.enumerateDevices());
-    setAudioDevices(devices);
-  }
-
-  const onDeviceSelected = async (device: MediaDeviceInfo) => {
-    const stream = await navigator.mediaDevices.getUserMedia({audio: { deviceId: { exact: device.deviceId }}});
-    console.log(stream);
-    setSelectedDevice(device);
-    audioStream.connectStream(stream);
-  };
-
-  useEffect(()=>{
-    refreshAudioDevices();
-  }, []);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      audioStream.tick()
-      setBands(audioStream.bands);
-      setTickNum(audioStream.tickNum);
-    }, 1000/INTERVAL_FPS);
-    return () => clearInterval(interval);
-  }, []);
 
   return (
     <div className="App">
       <ButtplugComp />
       <hr />
-      <MusicAnalyzer />
+      {/* <MusicAnalyzer /> */}
 
 
     </div>
--- src/components/buttplug/ButtplugComp.tsx
+++ src/components/buttplug/ButtplugComp.tsx
@@ -1,12 +1,25 @@
 import React, { useState, useEffect } from "react";
-import * as Buttplug from 'buttplug';
+import * as ButtplugIO from 'buttplug';
 import { ButtplugClient, ButtplugClientDevice } from "buttplug";
+
+interface IConnectedDeviceFeature {
+    index: number,
+    setIntensity: Function,
+}
+
+interface IConnectedDevice {
+    device: ButtplugClientDevice,
+    featureCount: number,
+    stepCount: number[],
+    maxDuration: number,
+    features: Array<IConnectedDeviceFeature>,
+}
 
 const ButtplugComp = () => {
 
     const [loaded, setLoaded] = useState(false);
 
-    const [devices, setDevices] = useState<Buttplug.ButtplugClientDevice[]>([]);
+    const [connectedDevices, setConnectedDevices] = useState<IConnectedDevice[]>([]);
 
     const [client, setClient] = useState<ButtplugClient>();
 
@@ -21,30 +34,47 @@ const ButtplugComp = () => {
         // "deviceadded" events on successful connect.
         client.addListener("deviceadded", (device) => {
             console.log(`Device Connected: ${device.Name}`);
-            setDevices([...devices, device]);
+            
             // console.log("Client currently knows about these devices:");
             // client.Devices.forEach((device) => console.log(`- ${device.Name}`));
+            const {featureCount, maxDuration, stepCount} = device.messageAttributes(ButtplugIO.ButtplugDeviceMessageType.VibrateCmd);
+  
+            const features = new Array(featureCount).fill(null).map((_, index) => ({
+                    index,
+                    setIntensity: (intensity: number) => setFeatureIntensity(device, index, intensity),
+                } as IConnectedDeviceFeature)
+            );
+
+            setConnectedDevices([
+                ...connectedDevices,
+                { 
+                    device,
+                    featureCount,
+                    maxDuration,
+                    stepCount,
+                    features,
+                } as IConnectedDevice
+            ]);
         });
         client.addListener("deviceremoved", (device) => console.log(`Device Removed: ${device.Name}`));
-
-        // // Usual embedded connector setup.
-        // const connector = new Buttplug.ButtplugEmbeddedConnectorOptions();
-        // await client.connect(connector);
-
-        // Now that everything is set up, we can scan.
         await client.startScanning();
     }
 
-    const prodDevice = (device: ButtplugClientDevice) => {
-        device.vibrate(0.1);
-        setTimeout(() => { device.vibrate(0) }, 200);
+    const prodFeature = (connectedDevice: IConnectedDevice, featureIndex: number) => {
+        const intensity = 0.5;
+        setFeatureIntensity(connectedDevice, featureIndex, intensity);
+        setTimeout(() => { setFeatureIntensity(connectedDevice, featureIndex, 0); }, 200);
+    }
+
+    const setFeatureIntensity = (connectedDevice: IConnectedDevice, featureIndex: number, intensity: number) => {
+        return connectedDevice.device.vibrate([ new ButtplugIO.VibrationCmd(featureIndex, intensity) ]);
     }
 
     useEffect(() => {
         const connectButtplug = async () => {
-            await Buttplug.buttplugInit();
-            const connector = new Buttplug.ButtplugEmbeddedConnectorOptions();
-            const client = new Buttplug.ButtplugClient("Buttplug Example Client");
+            await ButtplugIO.buttplugInit();
+            const connector = new ButtplugIO.ButtplugEmbeddedConnectorOptions();
+            const client = new ButtplugIO.ButtplugClient("Buttplug Example Client");
             await client.connect(connector);
             setClient(client);
             setLoaded(true);
@@ -57,9 +87,18 @@ const ButtplugComp = () => {
     if (loaded) {
         return (
             <div>
-                <h4>Loaded</h4>
+                <h4>Devices</h4>
                 <button onClick={scan}>Scan</button>
-                <p>{devices.map((device) => <div id={`device:${device.Index}`}>{device.Name} - <button onClick={() => prodDevice(device)}>Prod</button></div>)}</p>
+                <div>{
+                    connectedDevices.map(( connectedDevice ) => (
+                        <div key={`device:${connectedDevice.device.Index}`}>
+                            <h3>{connectedDevice.device.Name}</h3>
+                            { connectedDevice.features.map((feature) => (
+                                <button key={feature.index} onClick={() => prodFeature(connectedDevice, feature.index)}>Prod</button>
+                            ))}
+                        </div>
+                    ))
+                }</div>
             </div>
         );
     }
