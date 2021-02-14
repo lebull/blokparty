7e46763dc0561a3e903ca4a71deb1025f97ec68b
lebull <bulldogboy912000@yahoo.com>
2021-02-13 21:32:25 -0600

    Add icons

--- src/layout/navbar.tsx
+++ src/layout/navbar.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { AudioDevicePicker } from "../components/music/MusicAnalyzerWorker";
 import { ButtplugScanButton } from "../components/buttplug/Buttplug";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./navbar.scss";
+import { faCog } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
-    return(<div>
-        <AudioDevicePicker />
-        <ButtplugScanButton />
+    return(<div className="navbar">
+        {/* <ButtplugScanButton /> */}
+        <span className="spacer" />
+        {/* <AudioDevicePicker /> */}
+        <FontAwesomeIcon icon={faCog} className="icon"/>
     </div>);
 }
 
